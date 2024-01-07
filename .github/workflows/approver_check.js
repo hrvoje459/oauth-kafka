6459d23ae370c8ded17788c8e039ff8693edf43f
hrvojerom <hrvoje.rom@porsche.digital>
2024-01-07 23:59:32 +0100

    Testing

--- .github/workflows/approver_check.js
+++ .github/workflows/approver_check.js
@@ -1,3 +1,24 @@
 
+const fs = require('fs');
 
 console.log("Hello, World!");
+
+const fileName = '/home/runner/work/oauth-kafka/oauth-kafka/output/to_do.json';
+
+fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file ${fileName}:`, err);
+      return;
+    }
+  
+    try {
+      // Parse the JSON data
+      const jsonData = JSON.parse(data);
+  
+      // Access and print specific properties
+      console.log('Message:', jsonData);
+    } catch (jsonError) {
+      console.error(`Error parsing JSON from file ${fileName}:`, jsonError);
+    }
+  });
+  
